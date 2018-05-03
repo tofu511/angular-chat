bc2e49b63ddf5a2381de62c7cec5a2f63c8e15d4
tofu511 <mokmok511@gmail.com>
2018-05-03 19:34:23 +0900

    firebaseを利用するための設定を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +18,10 @@ import { CommentDatePipe } from './pipe/comment-date.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase), // firebaseの設定を渡す
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "YOUR API KEY",
+    authDomain: "angular-chat-f6169.firebaseapp.com",
+    databaseURL: "https://angular-chat-f6169.firebaseio.com",
+    projectId: "angular-chat-f6169",
+    storageBucket: "angular-chat-f6169.appspot.com",
+    messagingSenderId: "134202540003"
+  }
 };
