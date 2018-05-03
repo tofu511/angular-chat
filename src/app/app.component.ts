6155143a5700a7b6f1d58d436df0b12d426e198d
tofu511 <mokmok511@gmail.com>
2018-05-03 18:00:17 +0900

    モックとなるコメントデータとコメントクラスを作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/comment';
+
+// コメントのモックデータ
+const COMMENTS: Comment[] = [
+  { name: '竹井賢治', message: 'お疲れ様です!' },
+  { name: '竹井賢治', message: 'おはようございます！' },
+  { name: '五十嵐洋平', message: 'お疲れ様です!' },
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  comments = COMMENTS;
 }
---
+++ src/app/class/comment.ts
@@ -0,0 +1,4 @@
+export class Comment {
+  name: string;
+  message: string;
+}
