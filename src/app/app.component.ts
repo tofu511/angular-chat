9899f6305b4dfe3403fece49d31948d26c483815
tofu511 <mokmok511@gmail.com>
2018-05-03 18:20:45 +0900

    自ユーザーと他ユーザーの出し分けを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER: User = new User(1, '五十嵐洋平');
+const ANOTHER_USER: User = new User(2, '竹井賢治');
 
 // コメントのモックデータ
 const COMMENTS: Comment[] = [
-  { name: '竹井賢治', message: 'お疲れ様です!' },
-  { name: '竹井賢治', message: 'おはようございます！' },
-  { name: '五十嵐洋平', message: 'お疲れ様です!' },
+  new Comment(ANOTHER_USER, 'お疲れ様です!'),
+  new Comment(ANOTHER_USER, 'おはようございます!'),
+  new Comment(CURRENT_USER, 'お疲れ様です!'),
+  new Comment(CURRENT_USER, 'おやすみなさい！'),
 ];
 
 @Component({
@@ -15,4 +20,5 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
   comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
