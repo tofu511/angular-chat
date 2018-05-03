11fbf30ecde398c8d0fd0a59dbeff66ff0d024f2
tofu511 <mokmok511@gmail.com>
2018-05-03 18:48:47 +0900

    コメントに日付を持たせる処理を追加

--- src/app/class/comment.ts
+++ src/app/class/comment.ts
@@ -1,13 +1,16 @@
+import { format } from 'date-fns';
 import { User } from './user';
 
 export class Comment {
   user: User;
   initial: string;
   message: string;
+  date: string;
 
   constructor(user: User, message: string) {
     this.user = user;
     this.initial = user.name.slice(0, 1);
     this.message = message;
+    this.date = format(new Date()); // Unix timestampに日付を整形
   }
 }
