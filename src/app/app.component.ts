54d0c1342871ab64be230ec68ae151d6d64027e9
tofu511 <mokmok511@gmail.com>
2018-05-03 19:59:05 +0900

    realtime databaseのデータを取得し表示する処理を追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 const CURRENT_USER: User = new User(1, '五十嵐洋平');
 const ANOTHER_USER: User = new User(2, '竹井賢治');
@@ -19,9 +21,16 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  item: Observable<any>;
   comments = COMMENTS;
   currentUser = CURRENT_USER;
   content = '';
+
+  constructor(private db: AngularFireDatabase) {
+    // DBの内容を取得し、変更を監視する
+    this.item = db.object('/item').valueChanges();
+  }
 
   // コメントを最後に追加する
   addComment(comment: string): void {
