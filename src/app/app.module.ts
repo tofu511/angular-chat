0c80c152efd40c1d090c86369a66c08f20829cdc
tofu511 <mokmok511@gmail.com>
2018-05-04 16:21:00 +0900

    共通処理をshared.moduleに移動

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
-
 import { AppComponent } from './app.component';
-import { CommentDatePipe } from './pipe/comment-date.pipe';
 import { ChatComponent } from './chat/chat.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentDatePipe,
     ChatComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    SharedModule,
     AngularFireModule.initializeApp(environment.firebase), // firebaseの設定を渡す
     AngularFireDatabaseModule,
     AngularFireAuthModule
